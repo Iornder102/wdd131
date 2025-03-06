42ea149613e0e6819923a8baf68338c5f8730776
iornder102 <iorndermsughter@gmail.com>
2025-03-06 21:40:11 +0100

    add commits

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,3 @@
 const year = document.querySelector("#year");
 let LastModif = new Date(document.lastModified);
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
