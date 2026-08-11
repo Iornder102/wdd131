c9bf5165d650442c59c200963b82df95a7622703
iornder102 <iorndermsughter@gmail.com>
2026-08-11 14:08:15 +0100

    Commit changes

--- week02/scripts/scripts.js
+++ week02/scripts/scripts.js
@@ -3,6 +3,14 @@ const button = document.querySelector('button');
 const list = document.querySelector('list');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
+li.textContent=input.value;
 deleteButton.textContent='❌';
 li.append(deleteButton);
 list.append(li);
+button.addEventListener('click', function(){
+    
+})
+deleteButton.addEventListener('click', function () {
+  list.removeChild(li);
+  input.focus();
+});
